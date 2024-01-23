6c7137faf16c92d6048e06be74bcabf3ff1b471a
rerishi <rishav3220@gmail.com>
2024-01-23 15:21:21 +0530

    First Commit

---
+++ src/test/java/abc1/A.java
@@ -0,0 +1,30 @@
+package abc1;
+
+import org.testng.annotations.Test;
+
+public class A {
+	
+	@Test
+	public void a1()
+	{
+		System.out.println("aaaaaa1");
+	}
+	
+	@Test
+	public void a2()
+	{
+		System.out.println("aaaaaa2");
+	}
+	
+	@Test
+	public void a3()
+	{
+		System.out.println("aaaaaa3");
+	}
+	
+	@Test
+	public void a4()
+	{
+		System.out.println("aaaaaa4");
+	}
+}
---
+++ src/test/java/abc1/B.java
@@ -0,0 +1,30 @@
+package abc1;
+
+import org.testng.annotations.Test;
+
+public class B {
+
+	@Test
+	public void b1()
+	{
+		System.out.println("bbbbb1");
+	}
+	
+	@Test
+	public void b2()
+	{
+		System.out.println("bbbbb2");
+	}
+	
+	@Test
+	public void b3()
+	{
+		System.out.println("bbbbb3");
+	}
+	
+	@Test
+	public void b4()
+	{
+		System.out.println("bbbbb4");
+	}
+}
